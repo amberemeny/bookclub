500f037392205e3f320ea5ec8365230db4092d7a
Flynn <flynnwebdev@outlook.com>
2019-10-14 11:54:48 +1000

    Update book added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update]
+
   def index
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   # Display the form
   def new
-    @book = Book.new    
+    @book = Book.new
   end
 
   def create
@@ -17,7 +18,19 @@ class BooksController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to @book
+  end
+
   private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
 
   def book_params
     return params.require(:book).permit(:title, :pub_year, :author_id)
